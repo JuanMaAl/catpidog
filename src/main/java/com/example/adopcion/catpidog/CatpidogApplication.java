1a27c085b02efc815c44e3b0f37790475ff9e1fa
JuanMaAl <juanmaal1986@gmail.com>
2025-02-09 21:44:19 +0100

    Primer commit

---
+++ src/main/java/com/example/adopcion/catpidog/CatpidogApplication.java
@@ -0,0 +1,13 @@
+package com.example.adopcion.catpidog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatpidogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatpidogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adopcion/catpidog/CatpidogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adopcion.catpidog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatpidogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
