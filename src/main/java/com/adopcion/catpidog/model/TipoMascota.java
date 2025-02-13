82c291796f7658a4fe687d3e0a547a316e5509ba
JuanMaAl <juanmaal1986@gmail.com>
2025-02-13 16:20:44 +0100

    Clase TipoMascota

---
+++ src/main/java/com/adopcion/catpidog/model/TipoMascota.java
@@ -0,0 +1,33 @@
+package com.adopcion.catpidog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class TipoMascota {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	
+	// Getters and Setters 
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}	
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
